bd8f12b0aafa277028fe5e32adbf9881caf6e34f
gulshan-ara <gulshan3015@gmail.com>
2024-08-09 21:32:24 +0600

    Intro to JSX in react

--- App.js
+++ App.js
@@ -1,47 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// create a react element - means the h1 or other html tags
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React - 101"
-);
+// const heading = React.createElement("h1", {}, "Hello World");
+const heading = <h1>Hello World JSX</h1>;
 
-// nested react elements for nested tags
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  // siblings
-  [
-    React.createElement("div", { id: "child1" }, [
-      React.createElement(
-        "h1",
-        { id: "heading" },
-        "I'm a nested React element"
-      ),
-      React.createElement(
-        "h2",
-        { id: "sub-heading" },
-        "I'm the sibling of the heading in a nested React element"
-      ),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement(
-        "h1",
-        { id: "heading2" },
-        "I'm a nested React element - 2"
-      ),
-      React.createElement(
-        "h2",
-        { id: "sub-heading2" },
-        "I'm the sibling of the heading in a nested React element - version 2"
-      ),
-    ]),
-  ]
-);
-// create the root of React - everything that will render, will render inside this root
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// render element in root
-root.render(parent);
+root.render(heading);
