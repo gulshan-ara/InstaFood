cf815ae8f8df6989ddc1c52d784dfbd0ece79884
gulshan-ara <gulshan3015@gmail.com>
2024-08-09 22:42:00 +0600

    Functional component intro

--- App.js
+++ App.js
@@ -2,7 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // const heading = React.createElement("h1", {}, "Hello World");
-const heading = <h1>Hello World JSX</h1>;
+const Heading = () => <h1>Hello World JSX</h1>;
+
+function SubHeading() {
+  return <h3>I'm the SubHeading!!</h3>;
+}
+
+const Body = () => {
+  return (
+    <div>
+      <Heading />
+      <SubHeading />
+      <p> I'm just a lil paragraph.</p>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Body />);
