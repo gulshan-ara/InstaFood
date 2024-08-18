c0f0947240018f9662a6d8ce06e22c6d96baa521
gulshan-ara <gulshan3015@gmail.com>
2024-08-18 23:14:21 +0600

    Food app homepage starting UI

--- App.js
+++ App.js
@@ -2,21 +2,70 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // const heading = React.createElement("h1", {}, "Hello World");
-const Heading = () => <h1>Hello World JSX</h1>;
+/**
+ * Header
+ * --- Logo
+ * --- Nav Items
+ * Body
+ * --- Search
+ * --- RestaurantContainer
+ * --- ReataurantCard
+ * -------- Img
+ * -------- Name of Res, star rating, cuisine etc
+ * Footer
+ * --- Copyright
+ * --- Links
+ * --- Address
+ * --- Contact
+ */
 
-function SubHeading() {
-  return <h3>I'm the SubHeading!!</h3>;
-}
-
-const Body = () => {
+const Header = () => {
   return (
-    <div>
-      <Heading />
-      <SubHeading />
-      <p> I'm just a lil paragraph.</p>
+    <div className="header">
+      <div>
+        <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ2aT1ZYtozvj5xLlh6bw3Yg_D_eXEO8g6Eg&s" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <h3>Meghna Foods</h3>
+    </div>
+  );
+};
+
+const BodyContainer = () => {
+  return (
+    <div className="body-container">
+      <div className="search-container">
+        <input className="search" placeholder="Search for Restaurants..." />
+      </div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <BodyContainer />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Body />);
+root.render(<AppLayout />);
