681768f26576edfbb6ed2a2b97544057c28795a0
gulshan-ara <gulshan3015@gmail.com>
2024-08-20 19:15:21 +0600

    css added to restaurant container

--- App.js
+++ App.js
@@ -23,7 +23,10 @@ const Header = () => {
   return (
     <div className="header">
       <div>
-        <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ2aT1ZYtozvj5xLlh6bw3Yg_D_eXEO8g6Eg&s" />
+        <img
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ2aT1ZYtozvj5xLlh6bw3Yg_D_eXEO8g6Eg&s"
+        />
       </div>
       <div className="nav-items">
         <ul>
@@ -40,18 +43,36 @@ const Header = () => {
 const RestaurantCard = () => {
   return (
     <div className="res-card">
-      <h3>Meghna Foods</h3>
+      <img
+        className="res-card-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/fhrunldultsj6hvo7wdo"
+        alt="image"
+      />
+      <div className="res-card-info">
+        <h3 className="res-card-title">Meghna Foods</h3>
+        <p className="res-card-rating">⭐️ 4.8 * 20-25mins</p>
+        <p>Sweets, Ice Cream</p>
+        <p>Navrangpura</p>
+      </div>
     </div>
   );
 };
 
-const BodyContainer = () => {
+const RestaurantContainer = () => {
   return (
     <div className="body-container">
       <div className="search-container">
         <input className="search" placeholder="Search for Restaurants..." />
       </div>
       <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
         <RestaurantCard />
       </div>
     </div>
@@ -62,7 +83,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <BodyContainer />
+      <RestaurantContainer />
     </div>
   );
 };
