bdff81e92be3c18c79b7ffc708ee6dd587816813
gulshan-ara <gulshan3015@gmail.com>
2024-09-10 11:55:15 +0600

    Added github user api

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,23 @@
-import User from "./User";
+import React from "react";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>Hi, I'm the About page!!</h1>
-      <User name="Gulshan" location="Dhaka" contact="@gulshan3015" />
-      <UserClass name="Gulshan Class" location="Dhaka" contact="@gulshan3015" />
-    </div>
-  );
-};
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    console.log("parent mounted");
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hi, I'm the About page!!</h1>
+        <UserClass />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -4,16 +4,28 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-    }
+      userInfo: {
+        name: "",
+        location: "",
+        contact: "",
+      },
+    };
+  }
+
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/gulshan-ara");
+    const json = await data.json();
+    this.setState({
+      userInfo: json,
+    });
   }
 
   render() {
     return (
       <div>
-        <h2>Name: {this.props.name}</h2>
-        <h3>Location: {this.props.location}</h3>
-        <h4>Contact: {this.props.contact}</h4>
+        <h2>Name: {this.state.userInfo.name}</h2>
+        <h3>Location: {this.state.userInfo.location}</h3>
+        <h4>Contact: {this.state.userInfo.company}</h4>
       </div>
     );
   }
