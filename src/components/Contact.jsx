d721c8e506ddc485bfe9598bc614ededa5999c60
gulshan-ara <gulshan3015@gmail.com>
2024-09-30 23:17:10 +0600

    Added testing library and wrote first test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="text-center my-40 font-bold">Page Coming Soon!!!!</div>
+    <div className="text-center my-40 font-bold">
+      <h1>Contact Us Page</h1>
+    </div>
   );
 };
 
