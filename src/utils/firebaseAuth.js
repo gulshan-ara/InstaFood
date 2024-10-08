da9f86c951fa086b0fe859203ccf78201e81180c
gulshan-ara <gulshan3015@gmail.com>
2024-10-08 15:46:06 +0600

    tried to fix vercel dangerous site issue

---
+++ src/utils/firebaseAuth.js
@@ -0,0 +1,17 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "./firebaseConfig";
+
+export async function userSignUp(email, password) {
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export async function userSignIn(email, password) {
+  return await signInWithEmailAndPassword(auth, email, password);
+}
+
+export async function userSignOut() {
+  return await signOut(auth);
+}
