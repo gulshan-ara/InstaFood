05f662cb9b47be5308df57f67a9d2b03fc7889b0
gulshan-ara <gulshan3015@gmail.com>
2024-12-11 18:31:42 +0600

    resolving cors error

--- src/components/RestaurantContainer.jsx
+++ src/components/RestaurantContainer.jsx
@@ -32,7 +32,7 @@ const RestaurantContainer = () => {
     if (location) {
       setLoading(true);
       const data = await fetch(
-        `https://corsproxy.io/?https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
+        `https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
       );
       const jsonVal = await data.json();
       // Optional Chaining
